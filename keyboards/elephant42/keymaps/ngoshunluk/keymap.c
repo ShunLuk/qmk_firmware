f31f8d915963f5cd820d784ccd06e368182b7e18
Shun Luk <ngoshun.luk@gmail.com>
2022-01-20 10:27:05 +0100

    keymap further modified to elephant

--- keyboards/elephant42/keymaps/ngoshunluk/keymap.c
+++ keyboards/elephant42/keymaps/ngoshunluk/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------'
               KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                                      KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                     KC_NO, LT(2,KC_ESC), KC_SPC, KC_NO,  KC_NO, OSL(3), KC_BSPC, KC_NO
+                                      KC_NO,  OSL(2), KC_SPC, KC_ESC,     KC_ENT, KC_BSPC, OSL(3), KC_NO
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   ),
 
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------|
               KC_NO , KC_TAB, KC_S,   KC_R,   KC_G,                                       KC_LBRC, KC_QUOT, KC_RBRC, KC_MINS, KC_EQL,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                     KC_NO, LT(2,KC_BSPC), KC_SPC, KC_NO, KC_NO,  KC_ENT, OSL(3), KC_NO
+                                      KC_NO, OSL(2), KC_SPC, KC_ESC,      KC_ENT, KC_BSPC, OSL(3), KC_NO
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   ),
 
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------'
              KC_NO, LCTL(LSFT(KC_TAB)), KC_DOWN, LCTL(KC_W), KC_NO,                        KC_NO, KC_NO, KC_PGDN, KC_NO,  KC_NO,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                      KC_NO, KC_TRNS, KC_SPC, KC_NO,       KC_NO, KC_ENT, KC_BSPC, KC_NO
+                                      KC_NO, KC_TRNS, KC_SPC, KC_ESC,     KC_ENT, KC_BSPC, KC_TRNS, KC_NO
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   ),
 
@@ -68,19 +68,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------'
              KC_0,   KC_1,   KC_2,   KC_3,  KC_BSLS,                                     KC_LBRC, KC_AT, KC_AMPR, KC_DLR, KC_RBRC,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                      KC_NO,  KC_ESC, KC_SPC, KC_NO,        KC_NO, KC_ENT, OSL(4), KC_NO
+                                      KC_NO, KC_TRNS, KC_SPC, KC_ESC,     KC_ENT, KC_BSPC, OSL(4), KC_NO
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   ),
 
   [_FUN] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------.                                    ,-------+-------+-------+-------+-------+-------.
-      KC_NO , KC_F12,     KC_F7,     KC_F8,     KC_F9,     KC_NO,                          KC_NO, KC_INS, KC_DEL, KC_NO,  KC_NO, KC_NO,
+      KC_NO, KC_F12,  KC_F7,  KC_F8,  KC_F9,  KC_NO,                                       KC_NO, KC_INS, KC_DEL, KC_NO,  KC_NO, KC_NO,
   //|-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------|
-      KC_NO, KC_F11,     KC_F4,     KC_F5,     KC_F6,     KC_CAPS,                         KC_NO, KC_LSFT,KC_LCTL,KC_LALT, KC_LGUI,KC_NO,
+      KC_NO, KC_F11,  KC_F4,  KC_F5,  KC_F6, KC_CAPS,                                      KC_NO, KC_LSFT,KC_LCTL,KC_LALT, KC_LGUI,KC_NO,
   //`-------+-------+-------+-------+-------+-------|                                    |-------+-------+-------+-------+-------+-------'
-             KC_F10,     KC_F1,     KC_F2,     KC_F3,     KC_NO,                           KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,
+             KC_F10,  KC_F1,  KC_F2,  KC_F3, KC_NO,                                        KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,
   //        `-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------'
-                                      KC_NO, KC_ESC,  KC_SPC, KC_NO,      KC_NO, KC_ENT, KC_BSPC, KC_NO
+                                      KC_NO, KC_TRNS, KC_SPC, KC_ESC,     KC_ENT, KC_BSPC, KC_TRNS, KC_NO
   //                                `-------+-------+-------+-------'    `-------+-------+-------+-------'
   )
 };
